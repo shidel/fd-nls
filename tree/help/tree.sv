3258703a8abbecfe20deaa8f79fd81c59d9bbd47
Sebastian Rasmussen <sebras@gmail.com>
2022-01-07 16:35:26 +0100

    Add Swedish translation of tree.

---
+++ tree/help/tree.sv
@@ -0,0 +1,17 @@
+Detta „r readme:n f”r FreeDOS-programmet TREE
+
+Visar katalogstrukturen f”r en enhet eller en s”kv„g.
+
+Anv„ndning:  TREE [/A] [/F] [S™KVŽG]
+  /A     Anv„nd ASCII linjeritningstecken
+  /F     Visar filer i kataloger (ist„llet f”r enbart kataloger)
+  S™KVŽG Var katalogtr„det ska visas fr†n, standardv„rdet „r
+         den aktuella katalogen.
+
+Detta program har st”d f”r internationella tecken via biblioteket
+"Cats" (catopen/catgets implementation f”r DOS).  Du kan h„mta
+biblioteket "Cats" fr†n http://www.freedos.org/jhall/cats
+
+TREE distribueras under GNU GPL.  Se filen COPYING
+f”r vidare information.
+
