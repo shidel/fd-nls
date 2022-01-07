34b96f5381a68f6a80e3188b45c31abcbe0830eb
Sebastian Rasmussen <sebras@gmail.com>
2022-01-07 16:33:42 +0100

    Add Swedish translation of exe2bin.

---
+++ exe2bin/help/exe2bin.sv
@@ -0,0 +1,9 @@
+Open Watcom EXE-till-bin„r-konverterare Version 1.5
+K„llkod finns tillg„nglig under Sybase Open Watcom Public License.
+EXE2BIN [flaggor] exe_fil[.exe] [bin_fil]
+Flaggor:
+        /Q        undertryck informationsmeddelanden
+        /H        visa exe-huvud
+        /R        visa omlokaliseringar
+        /L=<seg>  omlokalisera exe_fil till segment <seg>
+        /X        ut”kat beteende, t.ex. filer > 64KB
