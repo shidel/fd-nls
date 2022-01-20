28f5491e7b6c8d487108039d7ac2a158fdb0f3a3
Sebastian Rasmussen <sebras@gmail.com>
2022-01-20 10:32:13 +0100

    Add Swedish translation of fdhelper.

---
+++ fdhelper/nls/welcome.sv
@@ -0,0 +1,3 @@
+AUTO_DONE=Behandling av uppstartsfiler klar /fCyan %1 /a7 och /fCyan %a /a7
+AUTO_HELP=Skriv /fWhite %1 /fGray f”r att f† hj„lp med kommandon och navigering.
+AUTO_WELCOME=V„kommentill operatigsystemet /fGreen %1 /fCyan %2 /fGray ( /s- /fYellow "%3" /fGray )
