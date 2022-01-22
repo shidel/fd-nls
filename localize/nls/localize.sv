c147eb2937bd517caff30c3fb990d8f54fbd94a8
Sebastian Rasmussen <sebras@gmail.com>
2022-01-22 08:40:25 +0100

    Add Swedish translation of localize.

---
+++ localize/nls/localize.sv
@@ -0,0 +1,3 @@
+99.0:Skriver ut lokaliserade meddelanden (fr†n localize.*)
+99.1:Skriver ut meddelande nummer x.y eller, om inte tillg„ngligt, texten efter @@@\r\n(och slutligen ekar [et.c], om n†got)
+99.2:Errorlevel: 0 om okej, 1 om ingen localize.* hittades eller inget meddelande x.y fanns inuti.
