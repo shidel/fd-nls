bdd0d6e3e880c96fa996a4571568928e0b7f825d
Sebastian Rasmussen <sebras@gmail.com>
2022-01-22 15:08:44 +0100

    Add Swedish translation of mirror.

---
+++ mirror/help/mirror.sv
@@ -0,0 +1,20 @@
+HELP.TXT
+
+Hj„lp:
+
+Detta program spelar in filpositionsinformation f”r eventuellt †terskapande
+via UNFORMAT.
+
+Syntax:
+
+MIRROR [enhet:]
+MIRROR [/PARTN]
+
+  /PARTN    Lagrar en s„kerhetskopia av partitionstabellerna i en PARNSAV.FIL
+              p† disketten i A:-enheten. Partitionstabellerna kan †terskapas
+	      med UNFORMAT version 0.8.
+
+
+
+
+
