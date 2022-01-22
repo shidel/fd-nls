16a813424f2ee8fd4515ff311be08056876f5e9f
Sebastian Rasmussen <sebras@gmail.com>
2022-01-22 15:10:31 +0100

    Add Swedish translation of usbdos.

---
+++ usbdos/help/dosusb.sv
@@ -0,0 +1,5 @@
+DOSUSB
+
+DOSUSB „r en drivrutin f”r vissa USB-styrkort med ”ppen k„llkod.
+F”r n„rvarande fungerar den enbart med UHCI-styrkort, d.v.s. USB 1.1
+Det finns „nnu inget st”d f”r USB 2.0 och USB 3.0.
