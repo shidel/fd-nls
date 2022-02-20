216ff4116991e229290272d869d74594b854edc2
Sebastian Rasmussen <sebras@gmail.com>
2022-02-20 19:48:24 +0100

    Add Swedish translation for imgedit.

---
+++ imgedit/help/imgview.sv
@@ -0,0 +1,14 @@
+Enkel bildvisare
+
+anv„ndning: IMGVIEW.EXE [filnamn] [filnamn] [...]
+
+    [filnamn]       Ange fil att visa.
+
+Endast begr„nsat st”d f”r icke-inbyggda filformat (som BMP).
+
+Under visning...
+
+    PgUp    F”reg†ende bild
+    PgDown  N„sta bild
+    Enter   Visa om
+    Escape  Avsluta
